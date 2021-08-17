06b67313a651af0a325e8d875315a44aa769e0c8
lijiaqi <lijiaqi@parallelfuture.cn>
2021-08-17 10:08:35 +0800

    update

--- src/views/ExcelUpload/ExcelUpload.tsx
+++ src/views/ExcelUpload/ExcelUpload.tsx
@@ -1,9 +1,22 @@
+import { makeStyles } from '@material-ui/core';
 import React from 'react'
 import Dropzone from 'react-dropzone'
 import API from '../../api/api'
 
+const useStyles = makeStyles((theme) => ({
+  dropZoneArea: {
+    background: '#004c807d',
+    padding: '8rem 2rem',
+    textAlign: 'center',
+    color: '#ffffff',
+    border: '1px dashed',
+  }
+}));
+
+
 
 const ExcelUpload:React.FC = () => {
+  const classes = useStyles()
   async function onDrop(file:any) {
     let rlt = await API.upload(file[0])
     console.log(rlt)
@@ -18,7 +31,7 @@ const ExcelUpload:React.FC = () => {
         {({getRootProps, getInputProps, isDragActive}) => {
           
           return (
-            <div {...getRootProps()}>
+            <div {...getRootProps()} className={classes.dropZoneArea}>
               <input {...getInputProps()} />
               {!isDragActive && 'Click here or drop a file to upload!'}
             </div>
