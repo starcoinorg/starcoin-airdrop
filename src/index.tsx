a86ca1e44397f85b836df6e771e982bbdd50d251
bing <tarvos21@gmail.com>
2021-11-11 11:16:30 +0800

    Import i18n and t props to the whole REACT app

--- src/index.tsx
+++ src/index.tsx
@@ -1,20 +1,28 @@
-import React from 'react';
+import './utils/i18n';
+import React, { Suspense } from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './views/App/App'
 import reportWebVitals from './reportWebVitals';
 import theme from './theme'
 import { ThemeProvider } from '@material-ui/core';
+import { CircularProgress } from '@material-ui/core';
 
+const Loader = () => (
+  <CircularProgress size={20}/>
+);
 
 ReactDOM.render(
   <React.StrictMode>
     <ThemeProvider theme={theme}>
-      <App />
+      <Suspense fallback={<Loader />}>
+        <App />
+      </Suspense>
     </ThemeProvider>
   </React.StrictMode>,
   document.getElementById('root')
 );
+
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
