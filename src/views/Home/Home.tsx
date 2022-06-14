0f100dbe97e385f8b1ccfc95909b12ff0f5b8996
wk3368 <wk3368@163.com>
2022-06-14 12:21:48 +0800

    add STAR logo

--- src/views/Home/Home.tsx
+++ src/views/Home/Home.tsx
@@ -346,7 +346,7 @@ const Home: React.FC = () => {
               <Grid item xs={4}>
                 <Box display="flex" alignItems="center">
                   <Box>
-                    <img alt="stc" className={classes.tokenIcon} src="/img/stc.svg" />
+                    <img alt="stc" className={classes.tokenIcon} src={`/img/${row.Symbol}.svg`} />
                   </Box>
                   <Box>
                     <Typography variant="subtitle2">{row.Symbol}</Typography>
